a07f7849bb5523d331ce1a359338e3ab5c65a396
Juan Vanegas <eatteer@gmail.com>
2022-10-11 16:44:24 -0500

    Rename degrees from spanish to english

--- sections/About/data.ts
+++ sections/About/data.ts
@@ -2,17 +2,17 @@ import { Education, Skill } from "./types";
 
 export const education: Education[] = [
   {
-    degree: 'Tecnólogo en Desarrollo de Software',
+    degree: 'Software Development Technologist',
     academy: 'Universidad Tecnológica de Pereira',
     period: 'In process',
   },
   {
-    degree: 'Técnico Profesional en Sistemas de Cómputo',
+    degree: 'Professional Technician in Computer Systems',
     academy: 'Fundación Universitaria Comfamiliar Risaralda',
     period: '2018 - 2019',
   },
   {
-    degree: 'Técnico en Diseño e Integración Multimedia',
+    degree: 'Multimedia Design and Integration Technician',
     academy: 'Servicio Nacional Aprendizaje SENA',
     period: '2015 - 2016',
   },
